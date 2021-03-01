de4b358c77b0c9889699b79a67f47e541381e1f8
Chirag Charania <chiragcharania@gmail.com>
2021-03-01 00:21:27 -0500

    Added UAlbany logo

--- js/common.js
+++ js/common.js
@@ -111,7 +111,7 @@ let addfooter = function (relativepath = ".") {
                                 '<div class="col-sm-12 col-sm-offset-0 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">'+
                                     '<a target="_blank" href="https://www.albany.edu/">'+
                                         '<img alt="University at Albany Logo" class=" img-responsive footer-logo" height="39"'+
-                                        'src="'+relativepath+'/assets/images/UAlbany-logo.png" typeof="Image" />'+
+                                        'src="'+relativepath+'assets/images/UAlbany-logo.png" typeof="Image" />'+
                                     '</a>'+
                                     '<p>'+
                                         '<a target="_blank" href="https://www.facebook.com/universityatalbany"><span class="fab fa-facebook-f footer-icon" role="img"'+
